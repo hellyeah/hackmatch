16a824728111af25de8b73a2617f5ac8271b0a66
gailees <gailees@umich.edu>
2013-10-06 06:10:58 -0400

    watch for undefined still

--- js/hackmatch-hackmit.js
+++ js/hackmatch-hackmit.js
@@ -88,7 +88,9 @@ function hackmatch($scope, angularFire) {
 		      //console.log($scope.sites);
 		      //console.log($scope.sites[1]);
 		      for (var i = 0; i < results.length; i++) {
+		      	console.log(results[i]);
 		      	$scope.sites.push(results[i]);
+
 		        //var sites[i] = results[i];
 		      }
 		        //$('iframe').attr("src", object.get('url'));
