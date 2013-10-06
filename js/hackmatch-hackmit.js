b6eec7cbec46a248bd05d6bdce692b9b771920c8
gailees <gailees@umich.edu>
2013-10-06 07:10:51 -0400

    watch for undefined still

--- js/hackmatch-hackmit.js
+++ js/hackmatch-hackmit.js
@@ -20,25 +20,25 @@ function hackmatch($scope, angularFire) {
     var QueryString = function () {
 	  // This function is anonymous, is executed immediately and 
 	  // the return value is assigned to QueryString!
-	  var query_string = {};
-	  var query = window.location.search.substring(1);
-	  var vars = query.split("&");
-	  for (var i=0;i<vars.length;i++) {
-	    var pair = vars[i].split("=");
-	    	// If first entry with this name
-	    if (typeof query_string[pair[0]] === "") {
-	      query_string[pair[0]] = pair[1];
-	    	// If second entry with this name
-	    } else if (typeof query_string[pair[0]] === "string") {
-	      var arr = [ query_string[pair[0]], pair[1] ];
-	      query_string[pair[0]] = arr;
-	    	// If third or later entry with this name
-	    } else {
-	      query_string[pair[0]].push(pair[1]);
-	    }
-	  } 
-	  if (query_string) {
-	    return _.toArray(query_string);
+	  if (window.location.search) {
+		  var query_string = {};
+		  var query = window.location.search.substring(1);
+		  var vars = query.split("&");
+		  for (var i=0;i<vars.length;i++) {
+		    var pair = vars[i].split("=");
+		    	// If first entry with this name
+		    if (typeof query_string[pair[0]] === "") {
+		      query_string[pair[0]] = pair[1];
+		    	// If second entry with this name
+		    } else if (typeof query_string[pair[0]] === "string") {
+		      var arr = [ query_string[pair[0]], pair[1] ];
+		      query_string[pair[0]] = arr;
+		    	// If third or later entry with this name
+		    } else {
+		      query_string[pair[0]].push(pair[1]);
+		    }
+		  }
+		  return _.toArray(query_string);
 	  }
 	  else {
 	  	return [""];
