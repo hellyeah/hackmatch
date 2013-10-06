cf17cc99ebb0a4b31236a202cadcf7f4f10fc1c7
gailees <gailees@umich.edu>
2013-10-06 05:37:49 -0400

    testing with blank tag

--- js/hackmatch-hackmit.js
+++ js/hackmatch-hackmit.js
@@ -70,7 +70,7 @@ function hackmatch($scope, angularFire) {
 	$scope.loadStartupSites = function () {
 		var TestSites = Parse.Object.extend("sponsorSites");
 		var query = new Parse.Query(TestSites);
-		query.containsAll("tags", ["hackmit"]);
+		query.containsAll("tags", [""]);
 		  query.find({
 		    success: function(results) {
 		      //alert("Successfully retrieved " + results.length + " sites.");
