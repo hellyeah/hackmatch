4efcb97528a3373c6c6b8c06b4d160b680dc1d4c
gailees <gailees@umich.edu>
2013-10-06 07:44:40 -0400

    changing hackmatch js

--- js/hackmatch.js
+++ js/hackmatch.js
@@ -22,10 +22,10 @@ function hackmatch($scope, angularFire) {
 		var TestSites = Parse.Object.extend("sponsorSites");
 		var query = new Parse.Query(TestSites);
 		//query.containsAll("tags", [QueryString]);
-		if (QueryString) {
-			console.log('passed');
-			query.containsAll("tags", [QueryString]);
-		}
+		//if (QueryString) {
+		//	console.log('passed');
+		//	query.containsAll("tags", [QueryString]);
+		//}
 		  query.find({
 		    success: function(results) {
 		      //alert("Successfully retrieved " + results.length + " sites.");
