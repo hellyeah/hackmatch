644b1bb4f32e84082730d23b7424188a2222fbb5
gailees <gailees@umich.edu>
2014-06-23 15:52:06 -0700

    implementing angular with terms page

---
+++ js/terms.js
@@ -0,0 +1,11 @@
+function Terms($scope) {
+    $scope.todos = [
+        {text:'Learn AngularJS', done:false}, 
+        {text:'Build an app', done:false}
+    ];
+
+    $scope.signUp = {
+        console.log('blah');
+    }
+
+}
